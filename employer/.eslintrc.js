22a4ea986c35cd137deb96d035f6b9ba2d54f4a6
tharuneshwar-s <s.tharuneshwar.369@gmail.com>
2025-08-14 09:39:30 +0530

    employer

--- employer/.eslintrc.js
+++ employer/.eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
   root: true,
   extends: [
-    'next',
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
+    "next",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
   ],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'prefer-const': 'warn',
-    '@typescript-eslint/no-empty-object-type': 'off',
-    'react/no-unescaped-entities': 'off',
+    "@typescript-eslint/no-explicit-any": "off", // covers all locations of 'Unexpected any. Specify a different type.'
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "prefer-const": "off",
+    "react/no-unescaped-entities": "off",
   },
 };
--- employer/next.config.ts
+++ employer/next.config.ts
@@ -1,6 +1,6 @@
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["pdf2json"],
+    serverExternalPackages: ["pdf2json"],
   },
   typescript: {
     ignoreBuildErrors: true,
@@ -12,8 +12,8 @@ const nextConfig = {
       "@typescript-eslint/no-explicit-any": "off", // covers all locations of 'Unexpected any. Specify a different type.'
       "@typescript-eslint/no-empty-interface": "off",
       "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "prefer-const": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "off",
       "react/no-unescaped-entities": "off",
     },
   },
